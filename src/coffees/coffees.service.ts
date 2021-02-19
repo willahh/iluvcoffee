b9aeccfdf2c83c02ce9c36bed5c3ad60c3eb821e
William Ravel <wravel@gmail.com>
2021-02-19 23:07:46 +0100

    Add class-transformer, class-validator and @nestjs/mapped-types

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +22,6 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    throw 'A random error';
     const coffee = this.coffees.find((item) => item.id === +id);
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -28,6 +32,7 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
