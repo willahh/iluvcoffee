bcd2ebbf032c5650a62fcc72d246181ea0947e21
William Ravel <wravel@gmail.com>
2021-02-19 13:15:58 +0100

    Add coffees service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffesController } from './coffes/coffes.controller';
+import { CoffeesService } from './coffees/coffees.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CoffesController],
+  providers: [AppService, CoffeesService],
 })
 export class AppModule {}
